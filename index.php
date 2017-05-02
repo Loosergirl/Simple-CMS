abeec94aeb75e965c7ef61ff6d1d4acfa46474e3
Martin Alversjö <martin.alversjo@icloud.com>
2017-05-02 14:33:14 +0200

    More header code.

--- index.php
+++ index.php
@@ -9,14 +9,19 @@
   <body>
     <header class="site-header">
       <div class="wrapper">
-      <nav class="site-header__nav">
-        <ul class="site-header__list">
-          <li id="float-lefty" class="site-header__listItem"><a href="blog.php">Name</a></li>
-          <li class="site-header__listItem"><a href="blog.php">Blog</a></li>
-          <li class="site-header__listItem"><a href="contact.php">Contact</a></li>
-          <li class="site-header__listItem">Register / Log in</li>
-        </ul>
-      </nav>
+        <div class="logo-left">
+          <h1 class="logo">Architecture</h1>
+          <p class="caption">Crazy building styles</p>
+        </div>
+
+        <nav class="site-header__nav">
+          <ul class="site-header__list">
+            <li id="float-lefty" class="site-header__listItem"><a href="blog.php">Name</a></li>
+            <li class="site-header__listItem"><a href="blog.php">Blog</a></li>
+            <li class="site-header__listItem"><a href="contact.php">Contact</a></li>
+            <li class="site-header__listItem">Register / Log in</li>
+          </ul>
+        </nav>
     </div>
     </header>
 
