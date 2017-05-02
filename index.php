0c31e3ede47bd7270f2b94501db9a27da4f96ef2
Martin Alversjö <martin.alversjo@icloud.com>
2017-05-02 13:55:04 +0200

    Added files + code

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Architect Blog</title>
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <header class="site-header">
+      <div class="wrapper">
+      <nav class="site-header__nav">
+        <ul class="site-header__list">
+          <li id="float-lefty" class="site-header__listItem"><a href="blog.php">Name</a></li>
+          <li class="site-header__listItem"><a href="blog.php">Blog</a></li>
+          <li class="site-header__listItem"><a href="contact.php">Contact</a></li>
+          <li class="site-header__listItem">Register / Log in</li>
+        </ul>
+      </nav>
+    </div>
+    </header>
+
+    <section class="jumbotron">
+      <img srcset="images/skyscraper-large1.png 1920w" alt="Photo of skyscraper">
+    </section>
+
+
+  </body>
+</html>
