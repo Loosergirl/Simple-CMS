c7edad2fcc680a92aafad082fbb0a2ff977240c4
Martin Alversjö <martin.alversjo@icloud.com>
2017-05-03 11:25:46 +0200

    Added Sass, gulp, filestructure for both css and php.

---
+++ partials/head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Architect Blog</title>
+    <link rel="stylesheet" href="css/main.css">
+  </head>
---
+++ partials/header.php
@@ -0,0 +1,21 @@
+  <body>
+    <header class="site-header">
+      <div class="wrapper">
+        <div class="site-header__logo-left">
+          <h1 class="site-header__logo">Architecture</h1>
+          <p class="site-header__logo-caption">Crazy building styles</p>
+        </div>
+
+        <nav class="site-header__nav">
+          <ul class="site-header__list">
+            <li id="float-lefty" class="site-header__listItem"><a href="blog.php">Name</a></li>
+            <li class="site-header__listItem"><a href="blog.php">Blog</a></li>
+            <li class="site-header__listItem"><a href="contact.php">Contact</a></li>
+            <li class="site-header__listItem">Register / Log in</li>
+          </ul>
+        </nav>
+    </div>
+    <section class="site-header__jumbotron">
+      <img srcset="images/skyscraper-large1.png 1920w" alt="Photo of skyscraper">
+    </section>
+    </header>
