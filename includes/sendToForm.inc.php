dcca7346505ad9763ffcb3cec268e374ae782717
rayzoor12 <emilwow22@gmail.com>
2017-05-21 21:04:35 +0200

    grejer

--- includes/sendToForm.inc.php
+++ includes/sendToForm.inc.php
@@ -1,17 +1,19 @@
 <?php 
 session_start();
+include '../partials/head.php';
 
 $ID = $_GET['ID'];
 
 if (isset($_SESSION['memberID'])){
   	?>
+  	<div class="edit-page__form">
 		<form action="/Simple-CMS/includes/updatePost.inc.php?ID=<?= $ID ?>" method="post">
 		    <input type="text" name="blogtitle" required placeholder="Write Title...">
 		    <input type="text" name="blogtext" required placeholder="Write Post..." >
 		    <input type="url" name="figure" required placeholder="Picture url" >
 		    <button type="submit">Post</button>
 		</form>
-
+	</div>
  <?php }
 
 
