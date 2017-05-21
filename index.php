d5524736f79fa459987f8b26d2a47d031e85fd57
rayzoor12 <emilwow22@gmail.com>
2017-05-21 17:32:22 +0200

    new post page

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
   session_start();
   include 'partials/head.php';
   include 'partials/header.php';
-  include 'partials/postForm.php';
   include 'partials/post-section.php';
   include 'partials/footer.php';
  ?>
