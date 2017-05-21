75cf77e70bf16f661e04c956a9f5c8e02342f4e0
rayzoor12 <emilwow22@gmail.com>
2017-05-21 21:31:39 +0200

    form klara

--- includes/sendToForm.inc.php
+++ includes/sendToForm.inc.php
@@ -6,10 +6,10 @@ $ID = $_GET['ID'];
 
 if (isset($_SESSION['memberID'])){
   	?>
-  	<div class="edit-page__form">
-		<form action="/Simple-CMS/includes/updatePost.inc.php?ID=<?= $ID ?>" method="post">
+  	<div class="edit-page__container">
+		<form class="edit-page__form" action="/Simple-CMS/includes/updatePost.inc.php?ID=<?= $ID ?>" method="post">
 		    <input type="text" name="blogtitle" required placeholder="Write Title...">
-		    <input type="text" name="blogtext" required placeholder="Write Post..." >
+		    <textarea type="text" name="blogtext" required placeholder="Write Post..." ></textarea>
 		    <input type="url" name="figure" required placeholder="Picture url" >
 		    <button type="submit">Post</button>
 		</form>
--- partials/postForm.php
+++ partials/postForm.php
@@ -4,12 +4,13 @@
 
   if (isset($_SESSION['memberID'])){
   	?>
-		<form action="/Simple-CMS/includes/blogpost.inc.php" method="post">
+  	<div class="edit-page__container2">
+		<form class="edit-page__form" action="/Simple-CMS/includes/blogpost.inc.php" method="post">
 		    <input type="text" name="blogtitle" required placeholder="Write Title..">
-		    <input type="text" name="blogtext" required placeholder="Write Post...">
+		    <textarea type="text" name="blogtext" required placeholder="Write Post..."></textarea>
 		    <input type="url" name="figure" required placeholder="Picture url">
 		    <button type="submit">Post</button>
 		</form>
-
+	</div>
  <?php }
  ?>
